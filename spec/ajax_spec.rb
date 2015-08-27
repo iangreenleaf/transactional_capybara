21084d7619295690efcae13d7d98d0c6cdb901d0
Ian Young <ian@iangreenleaf.com>
2015-08-26 22:16:25 -0700

    Test multiple sessions

--- spec/ajax_spec.rb
+++ spec/ajax_spec.rb
@@ -32,4 +32,43 @@ RSpec.describe "server with AJAX", type: :feature, js: true do
       expect(find(".message").text).not_to eq(@expected_message)
     end
   end
+
+  it "waits on all sessions" do
+    using_session :foo do
+      visit "/page_with_ajax"
+      expect(page).to have_content("Hello")
+    end
+
+    using_session :bar do
+      visit "/boring_page"
+      expect(page).to have_content("Hi")
+    end
+
+    Thread.fork do
+      sleep 0.5
+      AjaxServer.should_return_from_ajax = true
+    end
+    TransactionalCapybara::AjaxHelpers.wait_for_ajax(page)
+
+    using_session :foo do
+      expect(find(".message").text).to eq(@expected_message)
+    end
+
+    AjaxServer.should_return_from_ajax = false
+
+    using_session :bar do
+      visit "/page_with_ajax"
+      expect(page).to have_content("Hello")
+    end
+
+    Thread.fork do
+      sleep 0.5
+      AjaxServer.should_return_from_ajax = true
+    end
+    TransactionalCapybara::AjaxHelpers.wait_for_ajax(page)
+
+    using_session :bar do
+      expect(find(".message").text).to eq(@expected_message)
+    end
+  end
 end
--- spec/support/server.rb
+++ spec/support/server.rb
@@ -4,6 +4,10 @@ require_relative 'model'
 class AjaxServer < Sinatra::Base
   cattr_accessor :should_return_from_ajax
 
+  get "/boring_page" do
+    erb "Hi", layout: :basic
+  end
+
   get "/page_with_ajax" do
     erb :page_with_ajax, layout: :basic
   end
