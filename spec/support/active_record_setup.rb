df381366260803705d29966819908e0b39bd9965
Ian Young <ian@iangreenleaf.com>
2017-01-28 23:06:51 -0600

    Fold schema into ActiveRecord setup

--- spec/support/active_record_setup.rb
+++ spec/support/active_record_setup.rb
@@ -3,5 +3,11 @@ require_relative 'db_config'
 
 config = db_config('active_record')
 ActiveRecord::Base.establish_connection(config)
-load File.join(File.dirname(__FILE__), "schema.rb")
+
+ActiveRecord::Schema.define do
+  create_table :test_values, :force => true do |t|
+    t.string  :content
+  end
+end
+
 require_relative 'model/active_record'
