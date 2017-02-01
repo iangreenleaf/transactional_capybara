06d9b26f47fd47665dba42fc1c3db9a9e37a823a
Christoph Wagner <wagner@webit.de>
2017-02-01 09:43:06 +0100

    add warning on sequel connection sharing

--- lib/transactional_capybara/shared_connection/sequel.rb
+++ lib/transactional_capybara/shared_connection/sequel.rb
@@ -1,6 +1,6 @@
 module TransactionalCapybara
   module_function
   def share_connection
-    raise 'if you want to have connection sharing with sequel just use the connection option single_threaded: true' unless Sequel::Model.db.single_threaded?
+    warn 'WARNING: No database connection sharing enabled! You propably want to use the sequel connection option single_threaded: true' unless Sequel::Model.db.single_threaded?
   end
 end
